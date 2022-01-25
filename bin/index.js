3802eddf7a5e74e10b6dd6933b74b75729fdbf7a
iatopilskii <iatopilskii@dasreda.ru>
2022-01-25 16:15:56 +0300

    Add yargs for handling command-line arguments

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,12 @@
-#! /usr/bin/env node
-console.log('Billet creating')
+#!/usr/bin/env node
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+const usage = '\nUsage: billet <template_name> to create billet files from template'
+
+const options = yargs(hideBin(process.argv)).usage(usage)
+  .option('t', {alias: 'template', describe: 'Path to template file', type: 'string', demandOption: false})
+  .help(true)
+  .argv
+
+
